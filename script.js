e545a67d790f6c0ae881261b87d7f5dbf7d6e824
George Wu <georgieewuu@gmail.com>
2024-03-07 09:42:39 -0500

    walkthrough version

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Declare variables below to save the different divs of your story.
 
+// when option one button is clicked
+let optionOneButton = document.querySelector(".option-one");
+optionOneButton.addEventListener("click", function() {
+  // show option one screen
+  let optionOneScreen = document.querySelector(".option-one-screen");
+  console.log(optionOneScreen);
+  optionOneScreen.style.display = "block";
 
+  // hide the main screen
+  let storyOpeningScreen = document.querySelector(".story-opening-container");
+  storyOpeningScreen.style.display = "none";
+});
 
 
 
